abbb7464621db712970598f3685d88bb384196df
Sonali <variya.sonali19@gmail.com>
2021-09-05 22:01:28 +0930

    changed width for mobile screens

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 
 const Article = ({ date, title, paraText, backgroundColor }) => (
-  <Col xs={6} sm={4} className="infoCardColumn">
+  <Col xs sm={4} className="infoCardColumn">
     <Card p-5 style={{ backgroundColor: backgroundColor }}>
       <Card.Header> {moment(date).format("DD MMM, YYYY")}</Card.Header>
       <Card.Body>
