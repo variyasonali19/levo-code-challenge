35377dd390a70190a43a691148af12d555e9f359
Sonali <variya.sonali19@gmail.com>
2021-09-05 22:07:29 +0930

    updated dummy data

--- src/Components/APICall/APICall.jsx
+++ src/Components/APICall/APICall.jsx
@@ -7,14 +7,14 @@ const relatedArticles = [{
 },
 {
     date: "2020-06-28T12:37:05.637Z",
-    title: "Love of learning, art keys to a great year of Gwen",
+    title: "The best way to consume React-Bootstrap is via the npm package.",
     paraText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla libero cum illum neque voluptas sequi omnis expedita voluptate porro repudiandae aliquam quasi rem fuga ratione hic sapiente, minima a.",
     buttonTitle: "Read More",
     backgroundColor: "#E33371"
 },
 {
     date: "2020-02-15T12:37:05.637Z",
-    title: "Love of learning, art keys to a great year of Gwen",
+    title: "Doing so pulls in only the specific components that you use.",
     paraText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla libero cum illum neque voluptas sequi omnis expedita voluptate porro repudiandae aliquam quasi rem fuga ratione hic sapiente, minima a.",
     buttonTitle: "Read More",
     backgroundColor: "#FF9800"
@@ -35,14 +35,14 @@ const relatedArticles = [{
 },
 {
     date: "2020-03-03T12:37:05.637Z",
-    title: "Love of learning, art keys to a great year of Gwen",
+    title: "Which can significantly reduce the amount of code you end up sending to the client",
     paraText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla libero cum illum neque voluptas sequi omnis expedita voluptate porro repudiandae aliquam quasi rem fuga ratione hic sapiente, minima a.",
     buttonTitle: "Read More",
     backgroundColor: "#115293"
 },
 {
     date: "2020-03-12T12:37:05.637Z",
-    title: "Love of learning, art keys to a great year of Gwen",
+    title: "These bundles are available on unpkg, as well as in the npm package.",
     paraText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla libero cum illum neque voluptas sequi omnis expedita voluptate porro repudiandae aliquam quasi rem fuga ratione hic sapiente, minima a.",
     buttonTitle: "Read More",
     backgroundColor: "#4791DB"
@@ -56,7 +56,7 @@ const relatedArticles = [{
 },
 {
     date: "2020-06-28T12:37:05.637Z",
-    title: "Love of learning, art keys to a great year of Gwen",
+    title: "React-Bootstrap has started a repo with a few basic CodeSandbox examples.",
     paraText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum nulla libero cum illum neque voluptas sequi omnis expedita voluptate porro repudiandae aliquam quasi rem fuga ratione hic sapiente, minima a.",
     buttonTitle: "Read More",
     backgroundColor: "#E33371"
